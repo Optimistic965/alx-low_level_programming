a145dc5669ac8324bf1f47a50aeb721f3a62f918
Ifeoluwa <onabajoif@yahoo.com>
2022-10-13 20:39:57 -0700

    Working_with_c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - still the main function
- * Return: 0 with no error as return value
+ *main - main-block
+ *Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a peice of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
