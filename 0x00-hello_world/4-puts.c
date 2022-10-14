5c65963912e6174fb5dfc1521daa93230e2e74d2
Ifeoluwa <onabajoif@yahoo.com>
2022-10-13 19:38:48 -0700

    learning bout c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,7 @@
  * Return: returns 0
  */
 int main(void)
-{
-	
-	puts("Programming is like building a multilingual puzzel");
-	return (0);
+{	
+puts("\"Programming is like building a multilingual puzzel");
+return (0);
 }
