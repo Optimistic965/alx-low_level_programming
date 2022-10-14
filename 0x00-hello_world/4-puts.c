48097a02b27e90e2c28b93134044e97a68581bca
Ifeoluwa <onabajoif@yahoo.com>
2022-10-13 19:35:50 -0700

    learning bout compiler

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - the main function.
+ * (Prints a line of text)
+ * Return: returns 0
+ */
+int main(void)
+{
+	
+	puts("Programming is like building a multilingual puzzel");
+	return (0);
+}
--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,10 +1,10 @@
-#include <stdi0.h>
+#include <stdio.h>
 /**
  * main - print Hello world
  * Return: it's an int function thus, it returns 0
  */
 int main(void)
 {
-	printf("Hello world\n")
-	return(0)
+	printf("Hello world\n");
+	return(0);
 }
