401d70577522d4015a8eb8f4f75a3dfd84a13892
Ifeoluwa <onabajoif@yahoo.com>
2022-10-14 21:33:54 -0700

    If_else_while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * (short description comes here)
+ * Return: 0
+ */
+int main(void)
+{
+char i;
+i = '0';
+for (; i <= '9'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
