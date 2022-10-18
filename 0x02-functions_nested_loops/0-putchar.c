f8809689177d9d294a39ac6e56da9cfc457b70ef
Ifeoluwa <onabajoif@yahoo.com>
2022-10-18 11:58:45 -0700

    Functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - entry to the function
+ * (this program print _putchar)
+ * Return- thus return 0*/
+int main (void)
+{
+	char charater = "_putchar";
+
+	printf("%d", character);
+	return 0;
+}
