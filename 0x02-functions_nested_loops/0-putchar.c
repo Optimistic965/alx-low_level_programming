f6ab2647c7cc6f119fccde885deb65a80e999838
Ifeoluwa <onabajoif@yahoo.com>
2022-10-19 16:23:36 -0700

    updated_0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -13,11 +13,12 @@ int main(void)
 
 	while (i < 9)
 	{
-		char result = (char)(_putchar(wrd[i]));
+		char result = _putchar(wrd[i]);
 
 		printf("%c", result);
 		i++;
 	}
-
+	
+	printf("\n")
 	return (0);
 }
