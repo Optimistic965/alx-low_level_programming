4b59a581a7d41e30c4d9ee79e16124293d2ed971
Ifeoluwa <onabajoif@yahoo.com>
2022-10-19 16:53:11 -0700

    updated_1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include main.h
+
+/**
+ * main - entry to the main function
+ * (short description bout main)
+ * Return: thus return 0
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z') 
+	{
+		-putchar(ch);
+		ch++;
+	}
+	-putchar('\n');
+}
