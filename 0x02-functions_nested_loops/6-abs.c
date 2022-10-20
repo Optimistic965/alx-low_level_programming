bbd6aa4ee377eb8ea0e8ee0afac12563af363fff
Ifeoluwa <onabajoif@yahoo.com>
2022-10-19 17:53:20 -0700

    updated6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - function that convert any data type into an int
+ * @n - the int to be converted
+ * (short description bout what the function does)
+ * Return: just return int 0
+ */
+int _abs(int n)
+{
+	int a = (int) n;
+	return (a);
+}
