613c888396347fc1478414733954895528276780
Ifeoluwa <onabajoif@yahoo.com>
2022-10-19 18:56:58 -0700

    advancing

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print-last-digit - this prints the last digit of an number
+ * print_last_digit - this prints the last digit of an number
  * @n: the number to get it's last digit
  * Return: this returns the last digit of the argument
  */
