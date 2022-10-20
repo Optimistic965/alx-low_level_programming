2566bdea5c553149d900b9e692e8309804b4af3f
Ifeoluwa <onabajoif@yahoo.com>
2022-10-19 17:57:38 -0700

    updated6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,12 +2,21 @@
 
 /**
  * _abs - function that convert any data type into an int
- * @n - the int to be converted
+ * @r - the int to be converted
  * (short description bout what the function does)
  * Return: just return int 0
  */
 int _abs(int n)
 {
-	int a = (int) n;
+	int a;
+
+	if (n < 0)
+	{
+		a = (int)n * -1;
+	}
+	else
+	{
+		a = (int) n;
+	}
 	return (a);
 }
