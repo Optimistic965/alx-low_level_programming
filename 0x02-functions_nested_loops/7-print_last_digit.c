24845cadd690f0bf2979bef70102762b7bc5d8b7
Ifeoluwa <onabajoif@yahoo.com>
2022-10-19 18:12:50 -0700

    updated7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,14 @@
 int print_last_digit(int n)
 {
 	int last_digit;
+	int supp;
+
+	if (n < 0)
+	{
+		n = -n;
+	}
 
 	last_digit = n % 10;
-	return (last_digit);
+	supp = last_digit;
+	return (last_digit supp);
 }
