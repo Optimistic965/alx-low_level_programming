5c8a73f98c4ea0c22460e8350f070e502b86675a
Ifeoluwa <onabajoif@yahoo.com>
2022-10-19 17:09:30 -0700

    updated_1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print an alpahbet 10 times
+ * (short description bout function)
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char alpha = 'a';
+	int i = 0;
+
+	while (i < 10)
+	{
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+
+		i++;
+	}
+}
