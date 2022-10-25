79565292f7dce8f0d0c611c97294d29a84f5abfc
Ifeoluwa <onabajoif@yahoo.com>
2022-10-24 18:57:18 -0700

    more_functions

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,10 +4,10 @@
 /**
  * reset_to_98 - function to reset variable
  * (reset the value of a pointer to 98)
- * @*n: is a pointer
+ * @n: is a pointer
  * Return: void
  */
 void reset_to_98(int *n)
 {
-	printf("%d", *(n)=98);
+	*n = 98;
 }
