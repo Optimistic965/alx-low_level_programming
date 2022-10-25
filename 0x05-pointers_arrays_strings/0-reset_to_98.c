7eb3ed4889d8975d7ce67712945ed3100390df82
Ifeoluwa <onabajoif@yahoo.com>
2022-10-24 18:36:23 -0700

    woring-on-pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 
-/*
+/**
  * reset_to_98 - function to reset variable
  * (reset the value of a pointer to 98)
- * *n: is a pointer
+ * @*n: is a pointer
  * Return: void
  */
 void reset_to_98(int *n)
