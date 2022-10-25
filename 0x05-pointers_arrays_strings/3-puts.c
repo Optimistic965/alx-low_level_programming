35fb596fcb1a5d451f4d305d79ab644992d3683c
Ifeoluwa <onabajoif@yahoo.com>
2022-10-24 19:29:49 -0700

    0x05_pointer

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * _puts - print into output a string
+ * ( short decription)
+ * @str: bla bla
+ * Return: whatever
+ */
 void _puts(char *str)
 {
 	puts(str);
