dfc5bac8d1347be995c3246480f319d42f829e0c
Ifeoluwa <onabajoif@yahoo.com>
2022-10-24 19:20:22 -0700

    0x05_pointer

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - counts the length of a string
+ * ( this function counts the lenght of a string)
+ * @s: string to be counted
+ * Return: returns length of string
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+
+	return lenght;
+}
