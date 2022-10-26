a052e5a1db4de1d1f0d5f9177dbc27be54689491
Ifeoluwa <onabajoif@yahoo.com>
2022-10-26 13:28:41 -0700

    working_on_more_pointer

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - function that concatenate two strings
+ *  (short description bout the function)
+ *  @dest: varibale to be modified
+ *  @src: variable to be concated
+ *  Return: this return a charater been modified 
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	*dest = strcat( dest, src);
+
+	return ( *dest);
+}
