5f7f48f38ad6758c985ed046ec3a162965668de7
Ifeoluwa <onabajoif@yahoo.com>
2022-10-27 13:28:03 -0700

    working_on_pointer

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include "main.h"
 
-/*
+/**
 * _strncat - takes in three argument
 * ( function joins too strings together n numher of time)
 * @dest: destination string
@@ -10,7 +10,7 @@
 * @n: number of string to join from src
 * Return: function returns a char
 */
-char *_strncat( char *dest, char *src, int *n)
+char *_strncat( char *dest, char *src, int*n)
 {
 	strncat(dest, src, n);
 	return dest;
