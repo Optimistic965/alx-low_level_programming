96fffa8a64c0aa79123fe4fda87ad99773a7300e
Ifeoluwa <onabajoif@yahoo.com>
2022-11-02 06:51:54 -0700

    working_on_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print string to standard output
+ * @s: pointer to the string variable
+ * Return: it's a void function
+ */
+
+void _puts_recursion(char *s)
+{
+	if(*s == "\0")
+	{
+		_putchar("\n");
+	}
+
+	_putchar(*s);
+	_putchar(s + 1);
+}
