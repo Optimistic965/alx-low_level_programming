bb8e99fbf9ff28fbcfa7a98173ece822750bcea9
Ifeoluwa <onabajoif@yahoo.com>
2022-11-02 07:57:28 -0700

    working_on_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _print_rev_recursion - print a string in reverse
+ * @s: string to be reversed
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int len = strlen(*s);
+	int x = len - 1;
+	
+	if ( x < 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s[x]);
+	x--;
+	_print_rev_recursion(*s[x]);
+}
