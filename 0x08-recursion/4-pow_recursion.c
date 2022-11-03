761b128951de0f676615c55ba589becc5a86d2c5
Ifeoluwa <onabajoif@yahoo.com>
2022-11-03 03:25:09 -0700

    working_on_recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,7 +10,7 @@
 int _pow_recursion(int x, int y)
 {
 	int res;
-	
+
 	if (y < 0)
 	{
 		return (-1);
@@ -20,6 +20,6 @@ int _pow_recursion(int x, int y)
 		return (1);
 	}
 
-	res = x * _pow_recursion(x, (y -1));
+	res = x * _pow_recursion(x, (y - 1));
 	return (res);
 }
