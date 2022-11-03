90bda8aeb13d162057c988a93b05942202d40799
Ifeoluwa <onabajoif@yahoo.com>
2022-11-03 02:02:55 -0700

    working_on_recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * factorial - calculates teh factorial of a given positibe int
+ * @n: number to get it's factorial
+ * Return: an int is returned
+ */
+
+int factorial(int n)
+{
+	int res;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if ( n == 0)
+	{
+		return (1);
+	}
+
+	res = n * factorial(n - 1);
+
+	return (res);
+}
