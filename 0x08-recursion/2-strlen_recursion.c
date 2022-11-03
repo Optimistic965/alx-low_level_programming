dc2573103f253f30b6984d55e6790139d54ed1aa
Ifeoluwa <onabajoif@yahoo.com>
2022-11-03 01:49:28 -0700

    working_on_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen-recursion - count the munber of characters
+ *  in a string recursively.
+ *  @s: string to be counted.
+ *  Return: number of string.
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len;
+	
+	if( *s != '\0')
+	{
+		len = 1 + _strlen_recursion( s + 1);
+	}
+	else
+	{
+		return 0;
+	}
+
+	return (len);
+}
