af41e6eb2c208b456e35ec672559e17a8362c0be
Onabajo Ifeoluwa <51455353+Optimistic965@users.noreply.github.com>
2022-11-04 21:17:52 +0100

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,11 +1,11 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 /**
- * * numchecker - verifies if string has only numbers
- * * @s: string to check
- * * Description: return 1 if only numbers in string, 0 if not
- * * Return: 1 if only numbers, 0 if not
- * **/
+ * numchecker - verifies if string has only numbers
+ * @s: string to check
+ * Description: return 1 if only numbers in string, 0 if not
+ * Return: 1 if only numbers, 0 if not
+ */
 int numchecker(char *s)
 {
 	int i;
@@ -17,12 +17,12 @@ int numchecker(char *s)
 	return (1);
 }
 /**
- * * main - find least amount of coins needed for change
- * * @argc: number of parameters entered
- * * @argv: strings entered including file name
- * * Description: prints the minimum number of coins to make change
- * * Return: zero
- * **/
+ * main - find least amount of coins needed for change
+ * @argc: number of parameters entered
+ * @argv: strings entered including file name
+ * Description: prints the minimum number of coins to make change
+ * Return: zero
+ */
 int main(int argc, char *argv[])
 {
 	int change, coins;
