516f83d1f9d05618dbea9a6cb2e33ec3567c3337
Ifeoluwa <onabajoif@yahoo.com>
2022-11-04 07:30:45 -0700

    working_argument

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-
 /**
- * main - entry to the program
- * @argc: argument count
- * @argv: array of argument passed in
- * Return: integer, success
- */
-
+ * * main - prints name of file
+ * * @argc: number of arguments
+ * * @argv: arguments entered
+ * * Description: print name of file
+ * * Return: zero
+ * **/
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+	argc = argc;
 	return (0);
 }
