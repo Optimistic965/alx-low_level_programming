2e305dc037507229796d424d7c14832893656896
Ifeoluwa <onabajoif@yahoo.com>
2022-11-04 05:58:47 -0700

    working_argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry to the program
+ * @argc: argument count
+ * @argv: array of argument passed in
+ * Return: integer, success
+ */
+
+int main(int argc, char* argv)
+{
+	printf("%s", argv[0]);
+}
