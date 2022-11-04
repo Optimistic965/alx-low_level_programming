d64c2634b28be31a5acdac18c3ab8e1cc3a65a5e
Ifeoluwa <onabajoif@yahoo.com>
2022-11-04 13:12:11 -0700

    working_argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,24 +1,28 @@
-#include <stdio.h>
-
+nclude <stdio.h>
+#include <stdlib.h>
 /**
- * main - entry to the function
- * @argc: argument count
- * @argv: argument vector
- * Return: zero to indicate success
+ * main - product of 2 numbers
+ * @argc: first parameter
+ * @argv: second parameter
+ * DESCRIPTION: outputs the product if 2 numbers
+ * Return: Always (0) Success
  */
 
-int main(int argc, int* argv[])
+int main(int argc, char *argv[])
 {
-	int res;
-	
-	if (argc < 3)
+	int a, b, c, product;
+	char error[10] = "Error";
+	if (argc != 3)
 	{
-		printf("Error");
+		printf("%s\n", error);
 		return (1);
 	}
-
-	res = argv[1] * argv[2];
-	printf("%d\n", res);
-
+	for (a = 0; a < argc; a++)
+	{
+		b = atoi(*(argv + 1)); /**b is first number*/
+		c = atoi(argv[2]); /** c is second number*/
+	}
+	product = b *c;
+	printf("%d\n", product);
 	return (0);
 }
