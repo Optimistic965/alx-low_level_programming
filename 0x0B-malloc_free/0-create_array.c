15d40e4fc78cbaf20eb56f7694f8f55ee02abd76
Ifeoluwa <onabajoif@yahoo.com>
2022-11-08 18:21:31 -0800

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,8 +13,12 @@ char *create_array(unsigned int size, char c)
 {
 	char *p = malloc(size);
 	if (size == 0 || p == 0)
+	{
 		return (0);
+	}
 	while (size--)
+	{
 		p[size] = c;
 		return (p);
+	}
 }
