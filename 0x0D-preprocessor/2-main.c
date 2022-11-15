1f2e838d60df63d9af82555ab8aa82bc0e3c695c
Ifeoluwa <onabajoif@yahoo.com>
2022-11-15 00:26:19 -0800

    Functioning_macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry into the function
+ * (print name file it's compiled from)
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s", __FILE__);
+	return (0);
+}
